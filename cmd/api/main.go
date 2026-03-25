70af3aa028f7badf1cd1eb49a5827c48016e7ecd
FaisalDipto <dipto22d@gmail.com>
2026-03-26 04:38:44 +0600

    Initial commit: Professional API structure with Middleware

---
+++ cmd/api/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"todo-api/internal/database"
+	"todo-api/internal/handlers"
+	"todo-api/internal/middleware"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found, using system default variables")
+	}
+
+	dbUrl := os.Getenv("DB_URL")
+	port := os.Getenv("PORT")
+	
+	dbPool, err := database.Connect(dbUrl)
+	if err != nil {
+		log.Fatal("Could not connect to database:", err)
+	}
+	defer dbPool.Close()
+
+	logger := log.New(os.Stdout, "[TODO-API] ", log.LstdFlags)
+
+	h := &handlers.TodoHandler{
+		Pool: dbPool,
+		Logger: logger,
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/todos", h.HandleTodos)
+	mux.HandleFunc("/todos/", h.HandleTodosById)
+
+	loggedMux := middleware.Logging(logger)(mux)
+
+	fmt.Printf("API running on :%v...\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, loggedMux))
+}
---
+++ internal/handlers/todo.go
@@ -0,0 +1,118 @@
+package handlers
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+	"todo-api/internal/models"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type TodoHandler struct {
+	Pool *pgxpool.Pool
+	Logger *log.Logger
+}
+
+func (h *TodoHandler) HandleTodosById(w http.ResponseWriter, r *http.Request) {
+	idString := r.URL.Path[len("/todos/"):]
+	id , err := strconv.Atoi(idString)
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return
+	}
+
+	switch r.Method{
+	case http.MethodGet:
+		query := "SELECT id, title, status FROM todos WHERE id = $1"
+		var t models.Todo
+		err := h.Pool.QueryRow(context.Background(), query, id).Scan(&t.Id, &t.Title, &t.Status)
+		if err != nil {
+			http.Error(w, "Todo not found", 404)
+			return
+		}
+		json.NewEncoder(w).Encode(t)
+
+	case http.MethodDelete:
+		query := "DELETE FROM todos WHERE id = $1"
+		res, _ := h.Pool.Exec(context.Background(), query, id)
+		if res.RowsAffected() == 0 {
+			http.Error(w, "Todo not found.", 404)
+			return
+		}
+		w.WriteHeader(http.StatusNotFound)
+
+	case http.MethodPut:
+		var updateData struct{
+			Status bool `json:"status"`
+		}
+		query := "UPDATE todos SET status = $1 WHERE id = $2"
+		json.NewDecoder(r.Body).Decode(&updateData)
+		h.Pool.Exec(context.Background(), query, updateData.Status, id)
+		w.WriteHeader(http.StatusNoContent)
+	}
+}
+
+func (h *TodoHandler) HandleTodos(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		// ... (keep your existing GET logic here)
+		rows, err := h.Pool.Query(context.Background(), "SELECT id, title, status FROM todos")
+		if err != nil {
+		h.Logger.Printf("DATABASE ERROR: %v", err)
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+	defer rows.Close()
+
+	var todos []models.Todo = []models.Todo{}
+
+	for rows.Next() {
+		var t models.Todo
+		if err := rows.Scan(&t.Id, &t.Title, &t.Status); err != nil {
+			continue
+		}
+		todos = append(todos, t)
+	}
+
+	if err = rows.Err(); err != nil {
+			h.Logger.Printf("ROW ITERATION ERROR: %v", err)
+			http.Error(w, "Internal Server Error", 500)
+			return
+		}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(todos)
+
+	case http.MethodPost:
+		var newTodo models.Todo
+
+		if err := json.NewDecoder(r.Body).Decode(&newTodo); err != nil {
+			http.Error(w, "Invalid input", http.StatusBadRequest)
+			return
+		}
+
+		if newTodo.Title == "" {
+			http.Error(w, "Title is required", http.StatusBadRequest)
+			return
+		}
+
+		query := "INSERT INTO todos (title) VALUES ($1) RETURNING id"
+		err := h.Pool.QueryRow(context.Background(), query, newTodo.Title).Scan(&newTodo.Id)
+		if err != nil {
+			h.Logger.Printf("POST ERROR: %v", err)
+			http.Error(w, "Internal Server Error", 500)
+			return
+		}
+
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(newTodo)
+	
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
