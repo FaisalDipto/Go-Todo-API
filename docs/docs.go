c661db3f8714391b97359b8ed93dbc9db5d582ad
FaisalDipto <dipto22d@gmail.com>
2026-05-15 04:17:19 +0600

    Fix: regenerate swagger docs to remove hardcoded localhost

--- docs/docs.go
+++ docs/docs.go
@@ -399,6 +399,9 @@ const docTemplate = `{
                 "title"
             ],
             "properties": {
+                "deleted_at": {
+                    "type": "string"
+                },
                 "id": {
                     "type": "integer"
                 },
@@ -446,7 +449,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:9090",
+	Host:             "",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Todo API",
